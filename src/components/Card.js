c782e98c9d06299740ad5cdd1a0bdf7a4b790632
AR Developer <AR.Developer@accessretailpk.local>
2022-08-04 17:17:08 +0500

    Experience card added

---
+++ src/components/Card.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Card(){
+    return(
+        <div className="card">
+            <img src="/assets/images/card1.png" className="card--image" />
+            <div className="card--stats">
+                <img src="/assets/images/star.png" className="card--star"/>
+                <span>5.0</span>
+                <span className="gray">(6) • </span>
+                <span className="gray">USA</span>
+            </div>
+            <p>Life Lessons with Katie Zaferes</p>
+            <p><span className="bold">From $136</span> / person</p>
+        </div>
+    )
+}
